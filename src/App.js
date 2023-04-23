f7bb48c5590d8304e96d951d4a4ddb48704e6588
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-23 17:19:07 +0530

    added loding tab when page is fetching the data from the api

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
-import axios from "axios";
 
 function App() {
-  const [movies, setmovies] = useState([
-    {
-      episode_id: 2,
-      title: "Some Dummy Movie 2",
-      opening_crawl: "This is the second opening text of the movie",
-      release_date: "2021-05-19",
-    },
-  ]);
+  const [movies, setmovies] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
   async function fetchMovies() {
-    try {
-      const res = await axios.get("https://swapi.dev/api/films/");
-      setmovies(res.data.results);
-    } catch {
-      console.log("errorbackend");
-    }
+    setisLoading(true);
+    fetch("https://swapi.dev/api/films/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setmovies(data.results);
+        setisLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // try {
+    //   const res = await axios.get("https://swapi.dev/api/films/");
+    //   setmovies(res.data.results);
+    // } catch {
+    //   console.log("errorbackend");
+    // }
   }
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
+      {isLoading && (
+        <section>
+          <h1>is Loading</h1>
+        </section>
+      )}
       <section>
         <MoviesList movies={movies} />
       </section>
