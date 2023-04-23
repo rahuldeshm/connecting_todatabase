5a510be97104631f3c10c1dd9a27d6f3ed4abb69
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-23 19:39:15 +0530

    added error handling method along with reloading after five seconds

--- src/App.js
+++ src/App.js
@@ -5,20 +5,37 @@ import "./App.css";
 
 function App() {
   const [movies, setmovies] = useState([]);
+  const [retry, setRetry] = useState(true);
+  const [error, setError] = useState(null);
   const [isLoading, setisLoading] = useState(false);
+  function retryingHandler() {
+    setRetry(false);
+    console.log("stopped retrying");
+  }
+  function fetchMoviesAfterfivesec() {
+    if (retry) {
+      setTimeout(() => {
+        fetchMovies();
+      }, 5000);
+    }
+  }
   async function fetchMovies() {
     setisLoading(true);
-    fetch("https://swapi.dev/api/films/")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setmovies(data.results);
-        setisLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    setError(null);
+    try {
+      const res = await fetch("https://swapi.dev/api/films/");
+      if (!res.ok) {
+        throw new Error("Something went wrong ... retrying!");
+      }
+      const data = await res.json();
+      setmovies(data.results);
+    } catch (error) {
+      if (retry === true) {
+        fetchMoviesAfterfivesec();
+      }
+      setError(error.message);
+    }
+    setisLoading(false);
     // try {
     //   const res = await axios.get("https://swapi.dev/api/films/");
     //   setmovies(res.data.results);
@@ -26,18 +43,22 @@ function App() {
     //   console.log("errorbackend");
     // }
   }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
-      {isLoading && (
-        <section>
-          <h1>is Loading</h1>
-        </section>
-      )}
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <h1>is Loading</h1>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>found no movies</p>}
+        {!isLoading && error && (
+          <>
+            <p>{error}</p>{" "}
+            <button onClick={retryingHandler}>Stop retring</button>
+          </>
+        )}
       </section>
     </React.Fragment>
   );
